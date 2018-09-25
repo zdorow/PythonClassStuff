f3828a51f7fd88f59e026922928c955775a22fbb
Zach Dorow <zdorow@me.com>
2018-09-25 09:18:24 -0500

    Roman Numeral Lab

---
+++ RomanNumeral.py
@@ -0,0 +1,18 @@
+#######################################################################################
+#
+#     Program was written for the Advanced Programming class at Lakeland University
+#     Author: Zachary Dorow
+#     Teacher: Mr. Kevin Kurek
+#     Last Edited: Sept 14, 2018
+#
+#     Description: The program function is to take user input and validate then
+#       calculate tip,tax and then the total of the meal.
+#
+#######################################################################################
+# We are importing all of the tkinter library.
+from tkinter import *
+
+# Getting the GUI going.
+root = Tk()
+root.title("Roman Numeral Guide")
+root.configure(background='black')
